cd7b7dd1c959bcceb1c7048ff3ab908bd7c29bfc
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-10-15 17:47:44 +0530

    MergeSortedArray Done

---
+++ Array/MergeSortedArray.js
@@ -0,0 +1,43 @@
+// 88. Merge Sorted Array [Leetcode Problem]
+
+
+// 1st Approch 
+
+// var merge = function (nums1, m, nums2, n) {
+//     let nums1copy = nums1.slice(0, m);
+//     let p1 = 0;
+//     let p2 = 0;
+
+//     for (let i = 0; i < m + n; i++) {
+//         if (p2 >= n || (p1 < m && nums1copy[p1] < nums2[p2])) {
+//             nums1[i] = nums1copy[p1];
+//             p1++;
+//         }
+//         else {
+//             nums1[i] = nums2[p2]
+//             p2++;
+//         }
+//     }
+// };
+
+// 2nd Approch 
+
+
+var merge = function (nums1, m, nums2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1;
+
+    for (let i = m + n - 1; i >= 0; i--) {
+        if (p2 < 0) break;
+
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[i] = nums1[p1];
+            p1--;
+        }
+        else {
+            nums1[i] = nums2[p2];
+            p2--;
+
+        }
+    }
+};
