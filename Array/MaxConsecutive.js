d75bbca12391e1e5730876de6cde7befe244959d
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-10-16 18:52:05 +0530

    Done

---
+++ Array/MaxConsecutive.js
@@ -0,0 +1,14 @@
+var findMaxConsecutiveOnes = function (nums) {
+    let currentcount = 0;
+    let maxcount = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            currentcount++;
+        }
+        else {
+            maxcount = Math.max(currentcount, maxcount);
+            currentcount = 0;
+        }
+    }
+    return Math.max(maxcount, currentcount);
+};
