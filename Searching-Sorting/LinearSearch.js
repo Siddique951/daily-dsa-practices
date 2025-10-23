b0644a28db70ce4d0f404355653772098ef3900a
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-10-23 19:35:17 +0530

    BinarySearch and Linearsearch

---
+++ Searching-Sorting/LinearSearch.js
@@ -0,0 +1,15 @@
+
+let arr = [4, 9, 1, 0, 2];
+
+function linearSearch(arr, target) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1
+}
+
+let result = linearSearch(arr, 1);
+
+console.log(result)
