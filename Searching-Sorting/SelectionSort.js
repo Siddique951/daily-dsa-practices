d00112f1a6ab553714f6db700e8ecaaac7cd9636
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-10-27 20:01:43 +0530

    Sorting Done

--- Searching-Sorting/SelectionSort.js
+++ Searching-Sorting/SelectionSort.js
@@ -6,7 +6,7 @@ let arr = [7, 1, 5, 12, 0, 4, 3, 2];
 function selectionSort(a) {
     let n = a.length;
     for (let i = 0; i < n - 1; i++) {
-        //   find the miinimum in the array 
+        //   find the minimum in the array 
         let min = i;
         for (let j = i + 1; j < n; j++) {
             if (a[j] < a[min]) {
