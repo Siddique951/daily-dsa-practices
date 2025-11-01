29f32252cdf8b1df89055a11d17a07ea619aeabe
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-11-01 20:27:34 +0530

    Linked List

---
+++ LinkedList/ReverseLinkedList.js
@@ -0,0 +1,15 @@
+
+// 206: Reverse Linked List 
+
+var reverseList = function (head) {
+    let prev = null;
+    let curr = head;
+    while (curr) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    // head = prev;
+    return prev;
+};
