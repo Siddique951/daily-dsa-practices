b16402a40dfbaae74bab7a5ffe5bad6b23cf433e
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-11-02 20:40:49 +0530

    Linked List

---
+++ LinkedList/LinkedListCycle.js
@@ -0,0 +1,17 @@
+
+
+// Linked List Cycle [Leetcode Problem]
+
+
+var hasCycle = function (head) {
+    let seenNodes = new Set;
+    let curr = head;
+    while (curr != null) {
+        if (seenNodes.has(curr)) {
+            return true;
+        }
+        seenNodes.add(curr);
+        curr = curr.next;
+    }
+    return false;
+};
