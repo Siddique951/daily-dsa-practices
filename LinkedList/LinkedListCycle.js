939abb6cc6e30417dc3fd21b24125626a2ce6145
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-11-03 20:42:01 +0530

    Linked List

--- LinkedList/LinkedListCycle.js
+++ LinkedList/LinkedListCycle.js
@@ -15,3 +15,25 @@ var hasCycle = function (head) {
     }
     return false;
 };
+
+
+
+// 2nd Approch [Floyd's cycle finding Algorithm]
+
+
+var hasCycle = function (head) {
+    if (!head) return false;
+
+    let slow = head;
+    let fast = head.next;
+    while (slow != fast) {
+        if (fast === null || fast.next == null) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+};
+
+
