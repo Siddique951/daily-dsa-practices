aab05350d61048c4013090fb1a27328c0d943e07
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-11-04 21:48:20 +0530

    Linked List

---
+++ LinkedList/PalindromeLinkedList.js
@@ -0,0 +1,34 @@
+
+
+// Palindrome Linked List [Leetcode Problem]
+
+var isPalindrome = function (head) {
+    // Finding the middle element 
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // reverse the second half of the list 
+    let prev = null;
+    let curr = slow;
+    while (curr) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+    // checking the palindrome
+    let firstList = head;
+    let secondList = prev;
+    while (secondList) {
+        if (firstList.val != secondList.val) {
+            return false;
+        }
+        firstList = firstList.next;
+        secondList = secondList.next;
+    }
+    return true;
+};
