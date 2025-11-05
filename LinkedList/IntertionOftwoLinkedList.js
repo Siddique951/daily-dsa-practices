34be2fc9cee1cc82fa2a1ddcdaecac2de06b475f
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-11-05 21:22:50 +0530

    Linked List

---
+++ LinkedList/IntertionOftwoLinkedList.js
@@ -0,0 +1,18 @@
+
+// 160. Intersection of Two Linked Lists [Leetcode Problem]
+
+
+var getIntersectionNode = function (headA, headB) {
+    let store = new Set();
+    while (headB) {
+        store.add(headB);
+        headB = headB.next;
+    }
+    while (headA) {
+        if (store.has(headA)) {
+            return headA;
+        }
+        headA = headA.next;
+    }
+    return null;
+};
