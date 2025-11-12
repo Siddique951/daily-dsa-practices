afebfc1b6d22e3c0b816bba922eb1fb0338355f3
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-11-12 20:16:06 +0530

    Linked List

---
+++ LinkedList/RemoveNthNodeFromEndOfList.js
@@ -0,0 +1,23 @@
+
+// Remove Nth Node From End of List [Leetcode Porblem]
+
+var removeNthFromEnd = function (head, n) {
+    let sentinel = new ListNode();
+    sentinel.next = head;
+
+    let length = 0;
+    while (head) {
+        head = head.next;
+        length++;
+    }
+
+    let prevPos = length - n;
+
+    let prev = sentinel;
+    for (let i = 0; i < prevPos; i++) {
+        prev = prev.next;
+    }
+    prev.next = prev.next.next;
+
+    return sentinel.next;
+};
